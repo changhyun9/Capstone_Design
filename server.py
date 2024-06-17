59eb90bb6c9870cb6a8a5859ee7e9f80eb44fc9a
changhyun9 <ckdgus1713@naver.com>
2024-06-18 02:44:09 +0900

    Modify server.py - Refactoring code, add multi thread

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import json
-
 import torch
 import os
 import socket
@@ -13,14 +12,19 @@ import threading
 from datetime import datetime
 from ultralytics import YOLO
 
-class ServerSocket:
+model = YOLO('./best.pt')
+
+SERVER_IP = '10.43.24.186'
+SERVER_PORT = 8080
+
+RAS_IP = '192.169.0.203'
+RAS_PORT = 2018
+
+class Socket:
 
     def __init__(self, ip, port):
         self.TCP_IP = ip
         self.TCP_PORT = port
-        self.socketOpen()
-        self.receiveThread = threading.Thread(target=self.receiveImages)
-        self.receiveThread.start()
 
     def socketClose(self):
         self.sock.close()
@@ -36,40 +40,43 @@ class ServerSocket:
         print(u'Server socket [ TCP_IP: ' + self.TCP_IP + ', TCP_PORT: ' + str(self.TCP_PORT) + ' ] is connected with client')
 
     def receiveImages(self):
-
+        self.socketOpen()
         try:
             while True:
-                length = self.recvall(self.conn, 64)
-                length1 = length.decode('utf-8')
-                stringData = self.recvall(self.conn, int(length1))
-                stime = self.recvall(self.conn, 64)
-                print('send time: ' + stime.decode('utf-8'))
-                print('receive time: ' + datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f'))
+                length = self.recvall(self.conn, 64).decode('utf-8')
+                stringData = self.recvall(self.conn, int(length))
                 data = numpy.frombuffer(base64.b64decode(stringData), numpy.uint8)
                 decimg = cv2.imdecode(data, 1)
                 cv2.imwrite("saved_image.jpg", decimg)
+
                 results = model.predict("./saved_image.jpg", show=False)
+
                 for result in results:
-                    cls = result.boxes.cls
-                    if(cls == 0):
-                        value = {"r":255, "g":0, "b":0, "lx":50}
-                    elif(cls == 1):
+                    cls = int(result.boxes.cls[0])
+                    # cls = result.boxes.cls
+                    if (cls == 0):
+                        value = {"r": 255, "g": 0, "b": 0, "lx": 50}
+                    elif (cls == 1):
                         value = {"r": 0, "g": 255, "b": 0, "lx": 50}
-                    elif(cls == 2):
+                    elif (cls == 2):
                         value = {"r": 0, "g": 0, "b": 255, "lx": 50}
                     else:
                         print("No class")
+                        continue
+
+                    # change format from dict to json
+                    # create new thread for sending JSON data and start
                     json_data = json.dumps(value)
-                    self.sock.connect("192.168.0.203", 2018)
-                    self.sock.send(json_data.encode())
-                    print("Complete sending")
+                    thread = threading.Thread(target=send_socket, args=(json_data,))
+                    thread.start()
+
         except Exception as e:
             print(e)
             self.socketClose()
-            self.socketOpen()
+            # self.sockOpen()
             self.receiveThread = threading.Thread(target=self.receiveImages)
             self.receiveThread.start()
-        
+
     def recvall(self, sock, count):
         buf = b''
         while count:
@@ -79,10 +86,38 @@ class ServerSocket:
             count -= len(newbuf)
         return buf
 
+    def send_ras(self, data):
+        # implementation by ChatGPT
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((RAS_IP, RAS_PORT))
+            sock.send(data.encode())
+            print("Complete sending")
+
+# 1. Create Socket and Wait to connect client
+# 2. Call method receiveImages() and predict image through model
+# 3. Based on the predicted result, producing output
+# 4. Create new thread and Send the result to Raspberry pi
+def receive_jetson(self):
+    server = Socket(SERVER_IP, SERVER_PORT)
+    server.receiveImages()
+
+# plan to develop in second semester
+# def receive_application(self):
+
+# 1. Create Socket and Connect to client (Raspberry pi)
+# 2. Encode data JSON and send the result Raspberry pi
+def send_socket(self, data):
+    server = Socket(RAS_IP, RAS_PORT)
+    server.send_ras(data)
+
 def main():
-    global model 
-    model = YOLO('./best.pt')
-    server = ServerSocket('10.43.24.186', 8080)
+
+    # For Jetson Nano (After receiving images from client, predict result through model and send to Raspberry pi)
+    receive_socket = threading.Thread(target=receive_jetson)
+    receive_socket.start()
+
+    # For Application (After receiving Color temperature and Lux from client, send to Raspberry pi)
+    # plan to develop in second semester
 
 if __name__ == "__main__":
     main()
